da101afff84b1717afc9f6ed0070634d459337c3
PingjunChen <chenpingjun@gmx.com>
2018-08-12 11:45:48 -0400

    update package version

--- pycontour/__init__.py
+++ pycontour/__init__.py
@@ -4,9 +4,9 @@ import os, sys
 
 __all__ = ["PKG_DIR", "DATA_DIR", "__version__"]
 
+__version__ = '0.0.2'
 PKG_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(os.path.dirname(PKG_DIR), 'data')
-__version__ = '0.0.1'
 
 from .cv2_transform import *
 from .coor_transform import *
