ad513adc423e6513dd113af323ee30f0989cc944
PingjunChen <chenpingjun@gmx.com>
2018-08-31 13:43:53 -0400

    update version to 0.0.5

--- pycontour/__init__.py
+++ pycontour/__init__.py
@@ -4,7 +4,7 @@ import os, sys
 
 __all__ = ["PKG_DIR", "DATA_DIR", "__version__"]
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 PKG_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_DIR = os.path.join(os.path.dirname(PKG_DIR), 'data')
 
