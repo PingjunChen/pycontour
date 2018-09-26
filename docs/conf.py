a1d40986da00337bf4e1d93caeaa9efc80e42b90
PingjunChen <chenpingjun@gmx.com>
2018-09-26 02:56:53 -0400

    try Mocking out dependency

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,15 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
+if 'READTHEDOCS' in os.environ:
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+                return MagicMock()
+
+    MOCK_MODULES = ['shapely', 'shapely.wkt', 'shapely.wkb', 'shapely.geometry', 'shapely.ops']
+    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
