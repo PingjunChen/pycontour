1d51d3fe0d44dc5348b22de9869c2389e02e4b60
PingjunChen <chenpingjun@gmx.com>
2018-12-20 21:59:47 -0500

    add travis.yml

---
+++ test/test_coor_transform.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import numpy as np
+from os.path import dirname as opd
+from os.path import abspath as opa
+from os.path import join as opj
+TEST_PATH = opa(opd(__file__))
+PRJ_PATH = opd(TEST_PATH)
+sys.path.insert(0, opj(PRJ_PATH, "pycontour"))
+
+from pycontour import point_list_to_np_arr
+from pycontour import swap_wh
+from pycontour import np_arr_to_point_list
+
+
+def test_coor_transform():
+    point_list = [(0, 0), (2, 0), (2, 2), (1, 2)]
+    np_arr = point_list_to_np_arr(point_list)
+    swap_arr = swap_wh(np_arr)
+    swap_points = np_arr_to_point_list(swap_arr)
