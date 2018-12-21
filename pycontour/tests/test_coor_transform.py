337471095a4ab5b5d37ff350ce600116973cdad0
PingjunChen <chenpingjun@gmx.com>
2018-12-20 21:44:49 -0500

    update test_coor_transform.py

--- pycontour/tests/test_coor_transform.py
+++ pycontour/tests/test_coor_transform.py
@@ -8,7 +8,6 @@ from pycontour import np_arr_to_point_list
 
 
 if __name__ == "__main__":
-    import pdb; pdb.set_trace()
     point_list = [(0, 0), (2, 0), (2, 2), (1, 2)]
     np_arr = point_list_to_np_arr(point_list)
     swap_arr = swap_wh(np_arr)
