bbbb629c6007f5d02f6f367cfb867e44141dfacb
PingjunChen <chenpingjun@gmx.com>
2019-01-07 10:50:15 -0500

    update w/h and format in polygon module

--- pycontour/polygon/relation.py
+++ pycontour/polygon/relation.py
@@ -5,18 +5,21 @@ from shapely.geometry import Polygon
 
 from ..poly_transform import construct_poly_using_np_arr
 
-__all__ = ["contour_intersects", "construct_intersection_polygon",
-        "contour_contains"]
+
+__all__ = ["contour_intersects",
+           "construct_intersection_polygon",
+           "contour_contains"]
 
 
 def contour_intersects(np_arr1, np_arr2):
-    """Determine two contours are intersected or not
+    """ Determine two contours are intersected or not.
+
     Parameters
-    ----------
+    -------
     np_arr1 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
     np_arr2 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
 
     Returns
     -------
@@ -24,9 +27,11 @@ def contour_intersects(np_arr1, np_arr2):
         Check if two contours are intersected or not.
 
     """
+
     # Construct polygon from numpy array
     poly1 = construct_poly_using_np_arr(np_arr1)
     poly2 = construct_poly_using_np_arr(np_arr2)
+
     # Get convex_hull of polygon
     poly1 = poly1.convex_hull  # Get convex hull of poly
     poly2 = poly2.convex_hull
@@ -37,13 +42,14 @@ def contour_intersects(np_arr1, np_arr2):
 
 
 def construct_intersection_polygon(np_arr1, np_arr2):
-    """Construct polygon from the intersection part of two contours
+    """Construct polygon from the intersection part of two contours.
+
     Parameters
-    ----------
+    -------
     np_arr1 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
     np_arr2 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
 
     Returns
     -------
@@ -51,12 +57,15 @@ def construct_intersection_polygon(np_arr1, np_arr2):
         The intersection polygon of two given polygons
 
     """
+
     inter_flag = contour_intersects(np_arr1, np_arr2)
     if inter_flag == False:
         return None
+
     # Construct polygon from numpy array
     poly1 = construct_poly_using_np_arr(np_arr1)
     poly2 = construct_poly_using_np_arr(np_arr2)
+
     # Get convex_hull of polygon
     poly1 = poly1.convex_hull  # Get convex hull of poly
     poly2 = poly2.convex_hull
@@ -66,14 +75,16 @@ def construct_intersection_polygon(np_arr1, np_arr2):
     return inter_poly
 
 
+
 def contour_contains(np_arr1, np_arr2):
-    """Determine if contour of np_arr1 contains contour of np_arr2 or not
+    """Determine if contour of np_arr1 contains contour of np_arr2.
+
     Parameters
-    ----------
+    -------
     np_arr1 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
     np_arr2 : np.array
-        Contour with numpy array format
+        Contour with numpy 2d array format
 
     Returns
     -------
@@ -81,9 +92,11 @@ def contour_contains(np_arr1, np_arr2):
         Check if polygon 1 contains polygon 2 or not
 
     """
+
     # Construct polygon from numpy array
     poly1 = construct_poly_using_np_arr(np_arr1)
     poly2 = construct_poly_using_np_arr(np_arr2)
+
     # Get convex_hull of polygon
     poly1 = poly1.convex_hull  # Get convex hull of poly
     poly2 = poly2.convex_hull
