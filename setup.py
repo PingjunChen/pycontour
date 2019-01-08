d9482c08972723dd1b06103f2bb8ce3fffd473cb
PingjunChen <chenpingjun@gmx.com>
2019-01-08 11:28:03 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -7,26 +7,25 @@ import pycontour
 
 PKG_NAME = "pycontour"
 VERSION = pycontour.__version__
-DESCRIPTION = "Contour operation utilities."
+DESCRIPTION = "Contour toolkit in Python."
 HOMEPAGE = "https://github.com/PingjunChen/pycontour"
-LICENSE = "Apache"
+LICENSE = "MIT"
 AUTHOR_NAME = "Pingjun Chen"
 AUTHOR_EMAIL = "chenpingjun@gmx.com"
 
 REQS = ""
 with open('requirements.txt') as f:
-    REQS = [pkg.replace("==", ">=") for pkg in f.read().splitlines()] 
+    REQS = [pkg.replace("==", ">=") for pkg in f.read().splitlines()]
 
 CLASSIFIERS = [
-    'Development Status :: 1 - Planning',
+    'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
+    'Intended Audience :: Healthcare Industry',
     'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: Apache Software License',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Scientific/Engineering',
 ]
 
