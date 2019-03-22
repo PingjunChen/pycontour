b5bf86d9525d3b14a1be884c22e8bc5b4618d3a9
PingjunChen <chenpingjun@gmx.com>
2019-03-21 23:29:19 -0400

    add smooth function defination

--- pycontour/transform/smooth.py
+++ pycontour/transform/smooth.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+
+def smooth_cnt(np_arr, smooth_k=3):
+    """ Smooth the contour.
+
+    Parameters
+    -------
+    np_arr : np.array
+        contour with standard numpy 2d array format
+    smooth_k: int
+        smoothing parameter
+
+    Returns
+    -------
+    smooth_arr: np.array
+        smoothed contour
+
+    """
+
+    
