1e591056a348d529eccdf07ef0bab3ac4d4ba9de
PingjunChen <chenpingjun@gmx.com>
2019-03-26 00:28:51 -0400

    add crop mask sub image

--- pycontour/img/mask.py
+++ pycontour/img/mask.py
@@ -5,7 +5,10 @@ import cv2
 
 from ..cv2_transform import np_arr_to_cv_cnt
 
-__all__ = ["cnt_mask_img", ]
+
+__all__ = ["cnt_mask_img",
+           "cnt_mask_sub_img"]
+
 
 def cnt_mask_img(img, np_arr):
     """ Mask image by contour
@@ -36,3 +39,42 @@ def cnt_mask_img(img, np_arr):
     mask_img = img * mask
 
     return mask_img
+
+
+def cnt_mask_sub_img(img, np_arr):
+    """ Mask sub image using contour
+
+    Parameters
+    -------
+    img : np.array
+        image represented by numpy array
+    np_arr: np.array
+        contour with standard numpy 2d array format
+
+    Returns
+    -------
+    mask_sub_img: np.array
+        masked contour circumscribed sub-image
+
+    """
+
+    min_h, min_w = np.min(np_arr[0]), np.min(np_arr[1])
+    max_h, max_w = np.max(np_arr[0]), np.max(np_arr[1])
+    sub_img = img[min_h:max_h+1, min_w:max_w+1]
+    mask = np.zeros_like(sub_img, dtype=np.uint8)
+
+    shift_arr = np_arr.copy()
+    shift_arr[0] -= min_h
+    shift_arr[1] -= min_w
+
+    shift_cnt = np_arr_to_cv_cnt(shift_arr)
+    if len(sub_img.shape) == 2:
+        cv2.drawContours(mask, [shift_cnt], 0, 1, -1)
+    elif len(sub_img.shape) == 3 and sub_img.shape[2] == 3:
+        cv2.drawContours(mask, [shift_cnt], 0, [1, 1, 1], -1)
+    else:
+        raise NotImplementedError("Image shape not supported")
+
+    mask_sub_img = sub_img * mask
+
+    return mask_sub_img
--- test/img/test_mask.py
+++ test/img/test_mask.py
@@ -12,9 +12,9 @@ PRJ_PATH = opd(TEST_PATH)
 sys.path.insert(0, PRJ_PATH)
 sys.path.insert(0, opj(PRJ_PATH, "pycontour"))
 
-from pycontour.img import cnt_mask_img
+from pycontour.img import cnt_mask_img, cnt_mask_sub_img
 
-def test_mask_img():
+def test_cnt_mask_img():
     img_path = os.path.join(TEST_PATH, "data/Imgs/20181218042607.jpg")
     img = io.imread(img_path)
     np_arr = np.array([[300, 600, 700, 500, 400], [300, 400, 500, 600, 500]])
@@ -23,3 +23,13 @@ def test_mask_img():
     import matplotlib.pylab as plt
     plt.imshow(masked_img)
     # plt.show()
+
+def test_cnt_mask_sub_img():
+    img_path = os.path.join(TEST_PATH, "data/Imgs/20181218042607.jpg")
+    img = io.imread(img_path)
+    np_arr = np.array([[300, 600, 700, 500, 400], [300, 400, 500, 600, 500]])
+    masked_subimg = cnt_mask_sub_img(img, np_arr)
+
+    import matplotlib.pylab as plt
+    plt.imshow(masked_subimg)
+    plt.show()
