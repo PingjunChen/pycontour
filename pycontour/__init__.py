606ea3d53d828cb1fe1e0f23068f5c31dafdbe4e
PingjunChen <chenpingjun@gmx.com>
2019-06-06 16:06:32 -0400

    add submodule to __init__to enable more flexible import

--- pycontour/__init__.py
+++ pycontour/__init__.py
@@ -6,6 +6,12 @@ __all__ = ["__version__"]
 
 __version__ = '1.3.7'
 
+from . import cnt
+from . import fea
+from . import img
+from . import poly
+from . import rela
+from . import transform
 from .coor_transform import *
 from .cv2_transform import *
 from .poly_transform import *
